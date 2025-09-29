94405ba4f5bdd05a71978448c7b62624ab2ba24c
Seeka1-GIFT <cabdirxmn1100@gmail.com>
2025-09-29 06:52:17 -0700

    Update classes.js

--- shawedgym/backend/src/routes/classes.js
+++ shawedgym/backend/src/routes/classes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const classesController = require('../controllers/classesController');
 const authMiddleware = require('../middleware/auth');
-// Temporarily disable auth to allow frontend integration without login for classes.
-// router.use(authMiddleware);
+router.use(authMiddleware);
 
 router.get('/', classesController.getClasses);
 router.get('/:id', classesController.getClass);
